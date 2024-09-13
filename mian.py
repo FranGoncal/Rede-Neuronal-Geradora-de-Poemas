6b4b6bf0e851886e1d87d1a763a65611221c2ec4
FranGoncal <franciscogoncalvesep@gmail.com>
2024-09-13 11:53:43 +0100

    Formatar texto

--- mian.py
+++ mian.py
@@ -8,3 +8,15 @@ from tensorflow.keras.optimizers import RMSprop
 filepath = tf.keras.utils.get_file('shakespeare.txt', 'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
 text = open(filepath, 'rb').read().decode(encoding='utf-8').lower()
 
+text = text [300000:800000]
+
+characters = sorted(set(text))
+
+char_to_index = dict((c, i) for i, c in enumerate(characters))
+index_to_char = dict((i, c) for i, c in enumerate(characters))
+
+SEQ_LENGHT = 40
+STEP_SIZE = 3
+
+sentences = []
+next_characters = []
